0d4c59e1b91a370c56c18a5b44dbaf1e07d85eb2
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-04-26 08:33:51 +0200

    added API authentication

--- account/views.py
+++ account/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.csrf import csrf_exempt
+from tastypie import http
 
 from account.forms import SignUpForm
 
 
 def signup(request):
-
+    """ simple signup for users """
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -18,3 +21,18 @@ def signup(request):
         form = SignUpForm()
 
     return render(request, "account/signup.html", {'form': form})
+
+
+@csrf_exempt
+def api_auth(request):
+    """ authentication gate for the REST clients. Wraps the normal login method
+    into JSON shell """
+    if not request.method == "POST":
+        return http.HttpMethodNotAllowed()
+
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+        login(request, form.get_user())
+        return http.HttpAccepted()
+    else:
+        return http.HttpUnauthorized("Invalid credentials")
