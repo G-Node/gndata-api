67a95d4497eea70c7776cff3088b907c9702044b
Andrey Sobolev <sobolev.andrey@gmail.com>
2014-05-04 22:44:30 +0200

    patching custom sql adding CASCADE

--- metadata/sql/section.sql
+++ metadata/sql/section.sql
@@ -1,14 +1,15 @@
 --drop schema public cascade;
 --create schema public;
 
-ALTER TABLE metadata_document DROP CONSTRAINT metadata_document_pkey;
-ALTER TABLE metadata_document ADD PRIMARY KEY (guid);
+ALTER TABLE metadata_value DROP CONSTRAINT metadata_value_pkey CASCADE;
+ALTER TABLE metadata_value ADD PRIMARY KEY (guid);
 
-ALTER TABLE metadata_section DROP CONSTRAINT metadata_section_pkey;
-ALTER TABLE metadata_section ADD PRIMARY KEY (guid);
-
-ALTER TABLE metadata_property DROP CONSTRAINT metadata_property_pkey;
+ALTER TABLE metadata_property DROP CONSTRAINT metadata_property_pkey CASCADE;
 ALTER TABLE metadata_property ADD PRIMARY KEY (guid);
 
-ALTER TABLE metadata_value DROP CONSTRAINT metadata_value_pkey;
-ALTER TABLE metadata_value ADD PRIMARY KEY (guid);
+ALTER TABLE metadata_section DROP CONSTRAINT metadata_section_pkey CASCADE;
+ALTER TABLE metadata_section ADD PRIMARY KEY (guid);
+
+ALTER TABLE metadata_document DROP CONSTRAINT metadata_document_pkey CASCADE;
+ALTER TABLE metadata_document ADD PRIMARY KEY (guid);
+
